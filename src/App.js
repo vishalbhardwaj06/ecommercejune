f6ccc5223ead291f8d11391ec639291e1d59b70a
Vishal Bhardwaj <vishalbhardwaj5428@gmail.com>
2026-06-16 17:04:19 +0530

    react js latest

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Test, {User , xyz , Soul} from './Files/Home';
+ 
+function App() {
+  return (
+    <>
+      <h1>Hello {xyz}</h1>
+      <Test/>
+      <User />
+      <Soul/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Files/Home.js
@@ -0,0 +1,23 @@
+function Test(){
+  return(
+    <h1>Hii This is home</h1>
+  )
+}
+
+
+export function User(){
+  return(
+    <div>Hii I am user</div>
+  )
+}
+
+export function Soul(){
+  return(
+    <div>Hii I am Soul</div>
+  )
+}
+
+
+export const xyz = "i am XYZ"
+
+export default Test
